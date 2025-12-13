816097af4107f36e3e38514b5945c331006d9840
komaksym <kovalmaksym2@gmail.com>
2025-12-13 18:16:10 +0100

    add scaled_dot_prod_attn()

--- model/model.py
+++ model/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import math
 from einops import einsum, rearrange, reduce
 
 
@@ -8,12 +9,13 @@ class Linear(nn.Module):
     def __init__(self, in_features, out_features, device=None, dtype=None):
         super().__init__()
         # Specify mean for the param initialization
-        mean, std = 0, np.sqrt(2/(in_features+out_features))
+        mean, std = 0, np.sqrt(2 / (in_features + out_features))
         # Init the params from the normal distribution with said mean and std
-        param = torch.normal(mean=mean, std=std, size=(out_features, in_features),
-                             dtype=dtype, device=device)
+        param = torch.normal(
+            mean=mean, std=std, size=(out_features, in_features), dtype=dtype, device=device
+        )
         # Truncate
-        nn.init.trunc_normal_(param, a =-3 * std, b = 3 * std)
+        nn.init.trunc_normal_(param, a=-3 * std, b=3 * std)
         # Init the weight via the nn.Parameter
         self.weight = nn.Parameter(data=param)
 
@@ -27,13 +29,14 @@ class Embedding(nn.Module):
         # Specify mean for the param initialization
         mean, std = 0, 1
         # Init the params from the normal distribution with said mean and std
-        param = torch.normal(mean=mean, std=std, size=(num_embeddings, embedding_dim),
-                             dtype=dtype, device=device)
+        param = torch.normal(
+            mean=mean, std=std, size=(num_embeddings, embedding_dim), dtype=dtype, device=device
+        )
         # Truncate
         nn.init.trunc_normal_(param, a=-3, b=3)
         # Init the embedding via the nn.Parameter
         self.weight = nn.Parameter(data=param)
-    
+
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.weight[token_ids]
 
@@ -98,12 +101,14 @@ class RoPE(nn.Module):
         self.d_half = d_k // 2
         self.max_seq_len = int(max_seq_len)
         self.device = device if device is not None else torch.device("cpu")
-        
+
         j = torch.arange(self.d_half, dtype=torch.float32, device=self.device)
         inv_freq = self.theta ** (-2.0 * j / float(self.d_k))  # (d_half,)
-        pos = torch.arange(self.max_seq_len, dtype=torch.float32, device=self.device).unsqueeze(1)  # (max_seq_len,1)
+        pos = torch.arange(self.max_seq_len, dtype=torch.float32, device=self.device).unsqueeze(
+            1
+        )  # (max_seq_len,1)
         angles = pos * inv_freq.unsqueeze(0)  # (max_seq_len, d_half)
-        
+
         self.register_buffer("cos", torch.cos(angles))
         self.register_buffer("sin", torch.sin(angles))
 
@@ -111,18 +116,20 @@ class RoPE(nn.Module):
         if x.shape[-1] != self.d_k:
             raise ValueError(f"Last dim of x must be d_k={self.d_k}, got {x.shape[-1]}.")
         if token_positions.shape[-1] != x.shape[-2]:
-            raise ValueError("token_positions must have same seq_len in its last dim as x's sequence dimension.")
-        
+            raise ValueError(
+                "token_positions must have same seq_len in its last dim as x's sequence dimension."
+            )
+
         token_positions = token_positions.long().to(self.cos.device)
         cos_pos = self.cos[token_positions]  # (..., seq_len, d_half)
         sin_pos = self.sin[token_positions]
-        
+
         x_even = x[..., 0::2]  # (..., seq_len, d_half)
-        x_odd  = x[..., 1::2]  # (..., seq_len, d_half)
-        
+        x_odd = x[..., 1::2]  # (..., seq_len, d_half)
+
         x_rot_even = x_even * cos_pos - x_odd * sin_pos
-        x_rot_odd  = x_even * sin_pos + x_odd * cos_pos
-        
+        x_rot_odd = x_even * sin_pos + x_odd * cos_pos
+
         x_rot = torch.stack([x_rot_even, x_rot_odd], dim=-1)  # (..., seq_len, d_half, 2)
         new_shape = list(x.shape[:-2]) + [x.shape[-2], self.d_k]
         x_rot = x_rot.view(*new_shape)
@@ -134,3 +141,18 @@ def softmax(x, dim):
     x = x - torch.max(x, dim=dim).values.unsqueeze(-1)
     # Calculate softmax
     return torch.exp(x) / torch.sum(torch.exp(x), dim=dim).unsqueeze(-1)
+
+
+def scaled_dot_prod_attn(Q, K, V, mask=None):
+    d_k = Q.shape[-1]
+
+    # Calculate pre softmax
+    logits = torch.einsum("b...qd,b...kd->b...qk", Q, K) / math.sqrt(d_k)
+
+    # Apply the mask if exists
+    if mask is not None:
+        logits = logits.masked_fill(~mask, float("-inf"))
+
+    # Compute probs
+    scores = softmax(logits, dim=-1)
+    return torch.einsum("b...qk,b...kd->b...qd", scores, V)
