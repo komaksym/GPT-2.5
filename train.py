7caf439ea99fdde7e0fad4f8e87a76c1d2743456
komaksym <kovalmaksym2@gmail.com>
2026-02-07 19:44:55 +0100

    add download checkpoins command

--- train.py
+++ train.py
@@ -393,8 +393,11 @@ if __name__ == "__main__":
 
 """
 To run the script:
-WANDB_MODE=disabled uv run torchrun --nproc_per_node 1 train.py --batch_size 1 --grad_accum_steps 1 --context_length 1024 --num_layers 12 --d_model 768 --num_heads 12 --d_ff 2048 --theta 10000 --train_steps 20000 --lr 6e-4 --beta1 0.9 --beta2 0.95 --eps 1e-8 --weight_decay 0.1
+uv run torchrun --nproc_per_node 1 train.py --batch_size 1 --grad_accum_steps 1 --context_length 1024 --num_layers 12 --d_model 768 --num_heads 12 --d_ff 2048 --theta 10000 --train_steps 20000 --lr 6e-4 --beta1 0.9 --beta2 0.95 --eps 1e-8 --weight_decay 0.1
 
 To download the data:
 uv run hf download itskoma/GPT2.5 --repo-type dataset --local-dir .
+
+To download the checkpoints:
+uv run hf download itskoma/GPT2.5 --repo-type model --local-dir .
 """
