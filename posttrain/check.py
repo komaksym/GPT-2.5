efbaf0508bf21c7f97a2d92bf871278c8e16edec
komaksym <kovalmaksym2@gmail.com>
2026-02-25 17:23:03 +0100

    minor fix

--- posttrain/check.py
+++ posttrain/check.py
@@ -1,16 +1,20 @@
-from pretrain.model import load_checkpoint, TransformerLM, AdamW, is_distributed, TransformerBlock
-from pretrain.train import VOCAB_SIZE, setup
-from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
-from torch.distributed.fsdp import MixedPrecision
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from pretrain.model import load_checkpoint, TransformerLM, AdamW, is_distributed, TransformerBlock, generate
+#from pretrain.train import VOCAB_SIZE, setup
+#from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
+#from torch.distributed.fsdp import MixedPrecision
+#from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 import torch
-import os
-import functools
-import torch.distributed as dist
+from huggingface_hub import hf_hub_download
+import importlib
+import sys
+#import os
+#import functools
+#import torch.distributed as dist
 
 
 context_length = 1024
 num_layers = 12
+vocab_size = 50304
 d_model = 768
 num_heads = 12
 d_ff = 2048
@@ -21,61 +25,40 @@ weight_decay = 0.1
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 a_max = 18e-4 
 
+
 if __name__ == "__main__":
-    setup()
+    repo_id = "mikeawilliams/gpt2"
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_code_path = hf_hub_download(repo_id=repo_id, filename="model.py")
 
-    # FSDP
-    setup()
+    # Import the model module
+    spec = importlib.util.spec_from_file_location("model", model_code_path)
+    model_module = importlib.util.module_from_spec(spec)
+    sys.modules["model"] = model_module
+    spec.loader.exec_module(model_module)
 
-    local_rank, my_auto_wrap_policy, mp_policy = None, None, None
-    rank = dist.get_rank()
 
-    if is_distributed():
-        local_rank = int(os.environ["LOCAL_RANK"])
-        torch.cuda.set_device(local_rank)
+    checkpoint_path = hf_hub_download(repo_id=repo_id, filename="model_19072.pt")
+    state_dict = torch.load(checkpoint_path, weights_only=False, map_location="cpu")
 
-        my_auto_wrap_policy = functools.partial(
-            transformer_auto_wrap_policy,
-            transformer_layer_cls={TransformerBlock},
-        )
+    model = TransformerLM(vocab_size, context_length, num_layers, 
+                          d_model, num_heads, d_ff, 10000, device)
 
-        bf16_ready = (
-            torch.version.cuda and torch.cuda.is_bf16_supported() and dist.is_nccl_available()
-        )
+    # Load state dict
+    model.load_state_dict(state_dict["model"])
 
-        bfSixteen = MixedPrecision(
-            param_dtype=torch.bfloat16,
-            # Gradient communication precision.
-            reduce_dtype=torch.bfloat16,
-            # Buffer precision.
-            buffer_dtype=torch.bfloat16,
-        )
-
-        if bf16_ready:
-            mp_policy = bfSixteen
-        else:
-            mp_policy = None
-
-    model = TransformerLM(
-        VOCAB_SIZE, context_length, num_layers, d_model, num_heads, d_ff, theta, device=device
+    # Test
+    generated_sqs = generate(
+        "Once upon a time,",
+        max_tokens=50,
+        context_length=context_length,
+        batch_size=5,
+        model=model,
+        temp=0.8,
+        top_p=0.9,
+        device=device,
     )
 
-     # If in distributed mode
-    if rank is not None and transformer_auto_wrap_policy and mp_policy:
-        model = FSDP(
-            model.to(rank),
-            auto_wrap_policy=transformer_auto_wrap_policy,
-            mixed_precision=mp_policy,
-            device_id=torch.cuda.current_device(),
-            sync_module_states=True,
-        )
-
-    optimizer = AdamW(model.parameters(), a_max, betas, eps, weight_decay)
-    rank = 0
-    rank = dist.get_rank()
-    checkpoint_path = "checkpoint/final_checkpoint.pt"
-
-    it = load_checkpoint(checkpoint_path, model, optimizer, rank)
-    breakpoint()
+    for seq in generated_sqs:
+        # Add to the wandb table
+        print(seq)
