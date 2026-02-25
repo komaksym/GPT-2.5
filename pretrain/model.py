2929c876b98bbbd63ea367328f9d5d9f72dda6d5
komaksym <kovalmaksym2@gmail.com>
2026-02-25 17:48:15 +0100

    add AppState for DCP

--- pretrain/model.py
+++ pretrain/model.py
@@ -13,7 +13,9 @@ import torch.nn.functional as F
 from einops import rearrange, reduce
 from torch.distributed.checkpoint.state_dict import get_state_dict, set_state_dict
 from torch.distributed.fsdp import FullStateDictConfig, StateDictType
+from torch.distributed.checkpoint.stateful import Stateful
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.checkpoint.state_dict import StateDictOptions
 
 
 def is_distributed() -> bool:
@@ -584,6 +586,34 @@ def to_cpu(obj: typing.Any) -> typing.Any:
         return [to_cpu(v) for v in obj]
     else:
         return obj
+
+
+class AppState(Stateful):
+    """This is a useful wrapper for checkpointing the Application State. Since this object is compliant
+    with the Stateful protocol, DCP will automatically call state_dict/load_stat_dict as needed in the
+    dcp.save/load APIs.
+
+    Note: We take advantage of this wrapper to hande calling distributed state dict methods on the model
+    and optimizer.
+    """
+
+    def __init__(self, model, optimizer=None):
+        self.model = model
+        self.optimizer = optimizer
+
+    def state_dict(self):
+        # this line automatically manages FSDP FQN's, as well as sets the default state dict type to FSDP.SHARDED_STATE_DICT
+        model_state_dict, optimizer_state_dict = get_state_dict(self.model, self.optimizer)
+        return {
+            "model": model_state_dict, 
+            "optimizer": optimizer_state_dict
+        }
+
+    def load_state_dict(self, state_dict):
+        # sets our state dicts on the model and optimizer, now that we've loaded
+        set_state_dict(self.model, self.optimizer, 
+                       model_state_dict=state_dict["model"], 
+                       optim_state_dict=state_dict["optimizer"])
 
 
 def save_checkpoint(
