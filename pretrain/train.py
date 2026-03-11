19c46399731a0e910742d70a2f81448c8144e720
komaksym <kovalmaksym2@gmail.com>
2026-03-11 17:23:58 +0000

    scale logged loss down over ranks for global loss tracking

--- pretrain/train.py
+++ pretrain/train.py
@@ -82,10 +82,15 @@ def run_evaluation(
     with torch.autocast(device_type=device.type, dtype=torch.bfloat16):
         _, val_loss = model(val_inputs, val_labels)
 
+    # Scale loss down over ranks
+    val_loss_for_log = torch.tensor(val_loss)
+    dist.all_reduce(val_loss_for_log, op=dist.ReduceOp.SUM)
+    val_loss_for_log /= dist.get_world_size()
+
     if master_rank:
-        print(f"step {iteration + 1}, val loss: {val_loss.item()}")
+        print(f"step {iteration + 1}, val loss: {val_loss_for_log.item()}")
         # Log loss in wandb
-        run.log({"val_loss": val_loss.item()})
+        run.log({"val_loss": val_loss_for_log.item()})
 
     # Run generation
     generated_sqs = generate(
@@ -232,16 +237,21 @@ def training_together(
         end_event.record()
         # Wait for the GPU to reach end_event
         torch.cuda.synchronize()
-        step_time_ms = start_event.elapsed_time(end_event)
+        step_time_ms = start_event.elapsed_time(end_event, device=device)
         tokens_per_sec = (batch_size * context_length) / (step_time_ms / 1000)
-        perplexity = np.exp(loss_accum)
+        # Scale loss down over number of ranks
+        loss_for_log = torch.tensor(loss_accum)
+        dist.all_reduce(loss_for_log, op=dist.ReduceOp.SUM)
+        loss_for_log /= dist.get_world_size()
+        # Perplexity
+        perplexity = np.exp(loss_for_log)
         # Coordinated logging
         if master_rank:
             print(
-                f"step {i + 1}, loss: {loss_accum:.3f}, perp: {perplexity:.3f}, norm: {norm:.3f}, dt: {step_time_ms:.3f}, tok/s: {tokens_per_sec:.3f}"
+                f"step {i + 1}, loss: {loss_for_log:.3f}, perp: {perplexity:.3f}, norm: {norm:.3f}, dt: {step_time_ms:.3f}, tok/s: {tokens_per_sec:.3f}"
             )
             # Log loss in wandb
-            run.log({"loss": loss_accum, "perplexity": perplexity, "norm": norm, "lr": lr})
+            run.log({"loss": loss_for_log, "perplexity": perplexity, "norm": norm, "lr": lr})
             # Increment pbar
             pbar.update(1)
 
